a4d64f33ff313b4b38a0581935d04c597568c140
Mudit Dwivedi <112899978+Mudit-Dwivedi@users.noreply.github.com>
2024-11-24 21:28:11 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:4000/user/login', {
+      const response = await fetch('https://shopping-z604.onrender.com/user/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
